53908197379335d1f74d13459ba24c4361c640db
surya <surya.kiran2872@gmail.com>
2019-08-29 22:04:29 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup
+{
+name='googlesearch',
+url ='https://github.com/s-suryakiran/googlesearchsurya',
+author='suryakiran',
+author_email='surya.kiran2872@gmail.com',
+packages=['googlesearch'],
+install_requires=['urllib.request','math','re','bs4','pprint','threading','collections','time'],
+version='0.1',
+license='SURYA',
+description='You can search a url in google with python'
+}
